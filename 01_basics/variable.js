f75b02d8237519c4611fddfd93f2026b31bb25b5
Sandesh219 <141412898+Sandesh219@users.noreply.github.com>
2023-09-15 07:52:35 +0000

    variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId =12345
+let accountEmail = "sandeshshindesj@gmail.com"
+var accountPassword = "12345"
+accountCity= "kolhapur"
+let accountstate;
+ 
+// accountId = 2
+
+accountEmail = "sandesh@1077"
+accountPassword = "123456"
+accountCity = "Gadhinglaj"
+
+
+console.log(accountId);
+
+/*
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
